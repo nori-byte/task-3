2e390c89cda2e785edeb31f2a85a43c8fd4182bb
gr441_koaal <kulmanakovap@gmail.com>
2026-02-22 16:53:11 +0700

    add components

---
+++ vue/js/main.js
@@ -0,0 +1,23 @@
+Vue.component ('task-card', {
+    template: `
+<p>Task 1</p>
+`,
+    methods() {
+
+    },
+    computed() {
+
+    },
+});
+
+Vue.component ('task-column', {
+    data() {
+
+    },
+    template: `
+    `
+});
+
+new Vue({
+    el: '#app'
+});
